759d2cd8cfdbc4b825a09d8800d0f9e24f439431
Марк <75181378+markpreo@users.noreply.github.com>
2023-04-18 15:05:01 +0300

    Первоначальные файлы для проекта MakeCode

---
+++ main.ts
@@ -0,0 +1,24 @@
+radio.onReceivedString(function (receivedString) {
+    if (receivedString == "hello") {
+        basic.showString("Hello!")
+    }
+    if (receivedString == "right") {
+        maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 45)
+        maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CCW, 0)
+    }
+    if (receivedString == "left") {
+        maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 45)
+        maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CCW, 0)
+    }
+    if (receivedString == "forward") {
+        maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 255)
+    }
+    if (receivedString == "stop") {
+        maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 0)
+    }
+    if (receivedString == "back") {
+        maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CCW, 150)
+        maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CCW, 150)
+    }
+})
+radio.setGroup(1)
